9e155a5e601ada6063e450c95fe9517ab83968e0
Mikhail Abzalov <nachozzzz224@gmail.com>
2026-03-17 22:23:03 +0300

    Развернутый проект

---
+++ src/pages/WorkTable.tsx
@@ -0,0 +1,22 @@
+
+
+const WorkTable = () => {
+    return (
+        <>
+            <header className="header">
+                <nav className="header__nav header_nav">
+
+                </nav>
+                <button className="header_nav__button"></button>
+                <p className="greeting_user">Привет, Username</p>
+                <form className="search" role="search">
+                <input type="search" />
+                </form>
+            </header>
+
+            <main className="main-page">
+                    <h1></h1>
+            </main>
+        </>
+    )
+}
